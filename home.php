c170433ad60907967c95f0f28166470dc0df90bf
xhevier <marekh55@azet.sk>
2017-05-06 13:51:39 +0200

    Add files via upload

---
+++ home.php
@@ -0,0 +1,20 @@
+﻿<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+$this->load->helper('header');
+showHeader();
+$this->load->helper('menu');
+showMenuClient();
+	?>
+	<script>
+		$(document).ready(function(){
+			$("#menuHome").css("background-color", "#00B4D7");
+			$("#menuHome").css("color", "white");
+		});
+	</script>
+	<?php
+	echo '<div style="height:754px; background-color:white; text-align:center">';
+		echo '<img style="margin-top:200px"src="'.base_url('images/logo.png').'">';
+    echo '</div>';
+	
+$this->load->helper('footer');
