349d60037e4ef4d576ac6bb164ed09da12fe85ea
xhevier <marekh55@azet.sk>
2017-05-06 13:47:32 +0200

    Add files via upload

---
+++ home.php
@@ -0,0 +1,28 @@
+﻿<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+if ($this->session->userdata('permisson')==0  OR $this->session->userdata('permisson')==1) {
+$this->load->helper('header');
+showHeader();
+$this->load->helper('menu');
+showMenuAdmin();
+	?>
+	<script>
+		$(document).ready(function(){
+			$("#menuHome").css("background-color", "#00B4D7");
+			$("#menuHomeW").css("color", "white");
+		});
+	</script>
+	<?php
+	echo '<div style="height:754px; background-color:white; text-align:center">';
+		echo '<img style="margin-top:200px"src="'.base_url('images/logo.png').'">';
+    echo '</div>';
+	?>
+
+<?php $this->load->helper('footer');
+
+
+
+} else {
+    echo "error, neplatné privilégia";
+}
